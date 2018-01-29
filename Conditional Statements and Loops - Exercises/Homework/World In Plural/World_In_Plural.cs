d839fb99147a41ee4daf692eb4a3452cd575594b
Lyuboo <lyubomir.apostolov@gmail.com>
2018-01-30 00:12:26 +0200

    Update 4

---
+++ Conditional Statements and Loops - Exercises/Homework/World In Plural/World_In_Plural.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace World_In_Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+            string pluralWord = "";
+            if (inputWord.EndsWith("y"))
+            {
+                string cutWord = inputWord.Remove(inputWord.Length - 1);
+                pluralWord = cutWord + "ies";
+            }
+            else if (inputWord.EndsWith("o") || inputWord.EndsWith("ch") || inputWord.EndsWith("s") || inputWord.EndsWith("sh") || inputWord.EndsWith("x") || inputWord.EndsWith("z"))
+            {
+                pluralWord = inputWord + "es";
+            }
+            else
+            {
+                pluralWord = inputWord + "s";
+            }
+            Console.WriteLine(pluralWord);
+        }
+    }
+}
