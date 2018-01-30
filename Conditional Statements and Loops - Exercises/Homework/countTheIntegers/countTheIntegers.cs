393104dbc04a172c43a49a5a643d610aab8c3803
Lyuboo <lyubomir.apostolov@gmail.com>
2018-01-30 22:15:12 +0200

    Exercise 9 Count The Integers

---
+++ Conditional Statements and Loops - Exercises/Homework/countTheIntegers/countTheIntegers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace countTheIntegers
+{
+    class countTheIntegers
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (count = 0; ; count++)
+            {
+                try
+                {
+                    int inputNumber = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    break;
+                    //throw;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
