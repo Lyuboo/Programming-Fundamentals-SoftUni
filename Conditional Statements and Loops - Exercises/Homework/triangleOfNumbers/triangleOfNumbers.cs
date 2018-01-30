082d7b4e2d39a9071f774652baf062825847ffd1
Lyuboo <lyubomir.apostolov@gmail.com>
2018-01-30 22:23:56 +0200

    Exercise 10 Triangle of Numbers

---
+++ Conditional Statements and Loops - Exercises/Homework/triangleOfNumbers/triangleOfNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace triangleOfNumbers
+{
+    class triangleOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int counterHeight = 1; counterHeight <= inputNumber; counterHeight++)
+            {
+                for (int counterWidth = 0; counterWidth < counterHeight; counterWidth++)
+                {
+                    Console.Write($"{counterHeight} ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
