f44cd80ec558d8923a59187faf603f7052967cf4
Lyuboo <lyubomir.apostolov@gmail.com>
2018-01-30 22:05:09 +0200

    Exercise 8 Calories Counter

---
+++ Conditional Statements and Loops - Exercises/Homework/caloriesCounters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace caloriesCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfIngredients = int.Parse(Console.ReadLine());
+            int cheese = 500;
+            int tomatoSauce = 150;
+            int salami = 600;
+            int pepper = 50;
+            int totalCalories = 0;
+            for (int counter = 0; counter < numberOfIngredients; counter++)
+            {
+                string inputIngredient = Console.ReadLine().ToLower();
+                if (inputIngredient == "cheese")
+                {
+                    totalCalories = totalCalories + cheese;
+                }
+                else if (inputIngredient == "tomato sauce")
+                {
+                    totalCalories = totalCalories + tomatoSauce;
+                }
+                else if (inputIngredient == "salami")
+                {
+                    totalCalories = totalCalories + salami;
+                }
+                else if (inputIngredient == "pepper")
+                {
+                    totalCalories = totalCalories + pepper;
+                }
+                
+            }
+            Console.WriteLine($"Total calories: {totalCalories}");
+        }
+    }
+}
