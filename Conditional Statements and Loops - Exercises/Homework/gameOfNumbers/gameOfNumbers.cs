7d27df38f6029df1587866d17037a8f58300424c
Lyuboo <lyubomir.apostolov@gmail.com>
2018-01-31 00:42:59 +0200

    Exercises 13 & 14

---
+++ Conditional Statements and Loops - Exercises/Homework/gameOfNumbers/gameOfNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace gameOfNumbers
+{
+    class gameOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int inputNumberN = int.Parse(Console.ReadLine());
+            int inputNumberM = int.Parse(Console.ReadLine());
+            int inputMagicNumber = int.Parse(Console.ReadLine());
+            int combinationsCounter = 0;
+            int firstNumber = 0;
+            int secondNumber = 0;
+            for (int counter = inputNumberN; counter <= inputNumberM; counter++)
+            {
+                for (int check = inputNumberN; check <= inputNumberM; check++)
+                {
+                    if ((counter + check) == inputMagicNumber)
+                    {
+                        firstNumber = counter;
+                        secondNumber = check;
+                        
+                    }
+                    //Console.WriteLine(check);
+                    combinationsCounter++;
+                }
+            }
+            if (firstNumber == 0)
+            {
+                Console.WriteLine($"{combinationsCounter} combinations - neither equals {inputMagicNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Number found! {firstNumber} + {secondNumber} = {inputMagicNumber}");
+            }
+        }
+    }
+}
---
+++ Conditional Statements and Loops - Exercises/Homework/magicLetter/magicLetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace magicLetter
+{
+    class magicLetter
+    {
+        static void Main(string[] args)
+        {
+            char inputLetterOne = char.Parse(Console.ReadLine());
+            char inputLetterTwo = char.Parse(Console.ReadLine());
+            char inputLetterThree = char.Parse(Console.ReadLine());
+            for (char positionOne = inputLetterOne; positionOne <= inputLetterTwo; positionOne++)
+            {
+                for (char positionTwo = inputLetterOne; positionTwo <= inputLetterTwo; positionTwo++)
+                {
+                    for (char positionThree = inputLetterOne; positionThree <= inputLetterTwo; positionThree++)
+                    {
+                        if (positionOne != inputLetterThree && positionTwo != inputLetterThree && positionThree != inputLetterThree)
+                        {
+                            Console.Write($"{positionOne}{positionTwo}{positionThree} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
