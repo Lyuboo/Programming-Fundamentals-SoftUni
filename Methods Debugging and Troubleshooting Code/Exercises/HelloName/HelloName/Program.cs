bdbc179b766b09c45516454b84382f4c5ef8e6e9
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-01 17:52:05 +0200

    New Solution for today + Exercise 1 Hello Name

---
+++ Methods Debugging and Troubleshooting Code/Exercises/HelloName/HelloName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputName = Console.ReadLine();
+            WelcomeMessage(inputName);
+        }
+
+        private static void WelcomeMessage(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Methods Debugging and Troubleshooting Code/classExercise/classExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
