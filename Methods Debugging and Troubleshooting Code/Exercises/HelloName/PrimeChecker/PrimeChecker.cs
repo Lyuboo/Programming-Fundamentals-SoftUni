9d48082f7edcf7c6047bfbfed89590ba8c7257b7
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-01 20:27:58 +0200

    Exercise 6 PrimeChecker

---
+++ Methods Debugging and Troubleshooting Code/Exercises/HelloName/PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeChecker
+{
+    class PrimeChecker
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrimeNumber(number));
+        }
+
+        private static bool IsPrimeNumber(long number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return false;
+            }
+
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
