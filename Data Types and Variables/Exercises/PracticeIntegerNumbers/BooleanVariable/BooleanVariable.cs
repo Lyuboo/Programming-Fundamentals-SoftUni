685cfe38d74343d435145c34ae568c673a62cbcb
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-04 20:30:05 +0200

    Exercise 5 Boolean Variable

---
+++ Data Types and Variables/Exercises/PracticeIntegerNumbers/BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main(string[] args)
+        {
+            string InputBool = Console.ReadLine();
+            if (Convert.ToBoolean(InputBool))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
