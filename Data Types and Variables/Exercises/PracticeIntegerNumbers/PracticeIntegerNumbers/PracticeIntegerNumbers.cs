db807826501170367e51573d20eef8b8c382b902
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-04 19:00:19 +0200

    Exercise 1 Integer Numbers

---
+++ Data Types and Variables/Exercises/PracticeIntegerNumbers/PracticeIntegerNumbers/PracticeIntegerNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PracticeIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short input1 = short.Parse(Console.ReadLine());
+            byte input2 = byte.Parse(Console.ReadLine());
+            short input3 = short.Parse(Console.ReadLine());
+            int input4 = int.Parse(Console.ReadLine());
+            long input5 = long.Parse(Console.ReadLine());
+            long input6 = long.Parse(Console.ReadLine());
+            long input7 = long.Parse(Console.ReadLine());
+            Console.WriteLine(input1);
+            Console.WriteLine(input2);
+            Console.WriteLine(input3);
+            Console.WriteLine(input4);
+            Console.WriteLine(input5);
+            Console.WriteLine(input6);
+            Console.WriteLine(input7);
+        }
+    }
+}
