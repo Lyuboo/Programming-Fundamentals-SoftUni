9a055d6d2fae54b91ac39df54c5411ec8be9aeb7
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-04 19:24:11 +0200

    VariableInHEXFormat

---
+++ Data Types and Variables/Exercises/PracticeIntegerNumbers/VariableInHEXFormat/VariableInHEXFormat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariableInHEXFormat
+{
+    class VariableInHEXFormat
+    {
+        static void Main(string[] args)
+        {
+            string inputHEXNumber = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(inputHEXNumber, 16));
+        }
+    }
+}
