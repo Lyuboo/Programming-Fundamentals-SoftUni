2169cff086f15e14dbca71eba92ca8075b06dfb8
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-04 19:08:10 +0200

    Exercise 2 Floating Point Numbers

---
+++ Data Types and Variables/Exercises/PracticeIntegerNumbers/PracticeFloatingNumbers/PracticeFloatingNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PracticeFloatingNumbers
+{
+    class PracticeFloatingNumbers
+    {
+        static void Main(string[] args)
+        {
+            decimal input1 = decimal.Parse(Console.ReadLine());
+            double input2 = double.Parse(Console.ReadLine());
+            decimal input3 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(input1);
+            Console.WriteLine(input2);
+            Console.WriteLine(input3);
+        }
+    }
+}
