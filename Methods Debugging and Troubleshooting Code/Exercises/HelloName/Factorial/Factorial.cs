3934cdba4e7d65e849947471a807993d60c1568a
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-06 21:29:08 +0200

    Exercise 13 Factorial

---
+++ Methods Debugging and Troubleshooting Code/Exercises/HelloName/Factorial/Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            BigInteger inputNum = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFactorial(inputNum));
+        }
+
+        static BigInteger CalculateFactorial(BigInteger number)
+        {
+            BigInteger factorial = 1;
+
+            for (int currentNum = 1; currentNum <= number; currentNum++)
+            {
+                factorial *= currentNum;
+            }
+            return factorial;
+        }
+    }
+}
