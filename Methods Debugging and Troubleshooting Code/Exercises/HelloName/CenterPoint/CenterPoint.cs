32d25318333604c11575ebf7f596179ea96d1365
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-06 18:47:57 +0200

    CenterPoint

---
+++ Methods Debugging and Troubleshooting Code/Exercises/HelloName/CenterPoint/CenterPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CenterPoint
+{
+    class CenterPoint
+    {
+        static void Main(string[] args)
+        {
+            double inputX1 = double.Parse(Console.ReadLine());
+            double inputY1 = double.Parse(Console.ReadLine());
+            double inputX2 = double.Parse(Console.ReadLine());
+            double inputY2 = double.Parse(Console.ReadLine());
+
+            CenterPointPrint(inputX1, inputY1, inputX2, inputY2);
+        }
+
+        private static void CenterPointPrint(double x1, double y1, double x2, double y2)
+        {
+            double point1 = Math.Sqrt(x1 * x1 + y1 * y1);
+            double point2 = Math.Sqrt(x2 * x2 + y2 * y2);
+
+            if (point1 > point2)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+
+        }
+    }
+}
