2d229e972b6f4cc58b6792e904800b994c27461c
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-06 19:00:39 +0200

    Exercise 11 Geometry Calculator

---
+++ Methods Debugging and Troubleshooting Code/Exercises/HelloName/GeometryCalculator/GeometryCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GeometryCalculator
+{
+    class GeometryCalculator
+    {
+        static void Main(string[] args)
+        {
+            string inputFigure = Console.ReadLine();
+            double printValue = FigureArea(inputFigure);
+
+            Console.WriteLine($"{printValue:F2}");
+        }
+
+        static double FigureArea(string figure)
+        {
+            double area = 0.00;
+
+            switch (figure)
+            {
+                case "triangle":
+                    double side = double.Parse(Console.ReadLine());
+                    double height = double.Parse(Console.ReadLine());
+                    area = 0.5 * side * height;
+                    break;
+                case "square":
+                    side = double.Parse(Console.ReadLine());
+                    area = side * side;
+                    break;
+                case "rectangle":
+                    double width = double.Parse(Console.ReadLine());
+                    height = double.Parse(Console.ReadLine());
+                    area = width * height;
+                    break;
+                case "circle":
+                    double radius = double.Parse(Console.ReadLine());
+                    area = Math.PI * radius * radius;
+                    break;
+            }
+            return area;
+        }
+    }
+}
