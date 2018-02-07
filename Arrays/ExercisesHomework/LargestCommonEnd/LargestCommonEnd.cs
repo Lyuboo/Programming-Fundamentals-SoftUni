f109087b2dcae75f0bb473271364a0d08e8c0449
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-07 19:58:04 +0200

    New solution for Array exercise

---
+++ Arrays/ExercisesHomework/LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputArrayOne = Console.ReadLine();
+            string inputArrayTwo = Console.ReadLine();
+            string[] arrayOne = inputArrayOne.Split(' ');
+            string[] arrayTwo = inputArrayTwo.Split(' ');
+            int leftCounter = 0;
+            int rightCounter = 0;
+            int biggerSize = 0;
+
+            if (arrayOne.Length < arrayTwo.Length)
+            {
+                biggerSize = arrayTwo.Length;
+            }
+            else
+            {
+                biggerSize = arrayOne.Length;
+            }
+
+            for (int counter = 0; counter < biggerSize; counter++)
+            {
+                if (arrayOne[counter] == arrayTwo[counter])
+                {
+                    leftCounter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+              
+
+        }
+    }
+}
