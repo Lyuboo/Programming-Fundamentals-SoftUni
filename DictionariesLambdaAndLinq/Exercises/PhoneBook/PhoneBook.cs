a732ee043a9839f991ac7c44a13c637b373a5760
Lyuboo <lyubomir.apostolov@gmail.com>
2018-02-13 20:28:16 +0200

    Exercise 1 & 2

---
+++ DictionariesLambdaAndLinq/Exercises/PhoneBook/PhoneBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        static void Main()
+        {
+
+            var phoneBook = new Dictionary<string, string>();
+            string[] inputValue;
+
+            do
+            {
+                inputValue = Console.ReadLine().Split(' ');
+                if (inputValue[0] == "A")
+                {
+                    phoneBook[inputValue[1]] = inputValue[2];
+                }
+                if (inputValue[0] == "S")
+                {
+                    if (phoneBook.ContainsKey(inputValue[1]))
+                    {
+                        Console.WriteLine($"{inputValue[1]} -> {phoneBook[inputValue[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {inputValue[1]} does not exist.");
+                    }
+                }
+            } while (inputValue[0] != "END");
+            
+        }
+    }
+}
---
+++ DictionariesLambdaAndLinq/Exercises/PhoneBookUpgrade/PhoneBookUpgrade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        static void Main()
+        {
+
+            var phoneBook = new Dictionary<string, string>();
+            string[] inputValue;
+
+            do
+            {
+                inputValue = Console.ReadLine().Split(' ');
+                if (inputValue[0] == "A")
+                {
+                    phoneBook[inputValue[1]] = inputValue[2];
+                }
+                if (inputValue[0] == "S")
+                {
+                    if (phoneBook.ContainsKey(inputValue[1]))
+                    {
+                        Console.WriteLine($"{inputValue[1]} -> {phoneBook[inputValue[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {inputValue[1]} does not exist.");
+                    }
+                }
+                if (inputValue[0] == "ListAll")
+                {
+                    foreach (var contact in phoneBook.OrderBy(n => n.Key))
+                    {
+                        Console.WriteLine($"{contact.Key} -> {contact.Value}");
+                    }
+                }
+            } while (inputValue[0] != "END");
+
+        }
+    }
+}
